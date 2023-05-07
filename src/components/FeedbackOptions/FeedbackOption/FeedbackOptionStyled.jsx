28459481046bcfbcee5b6e938c39696aa19adab7
Andriy Karpishyn <makaron4eg@gmail.com>
2023-05-07 15:27:06 +0300

    Step 4 done

--- src/components/FeedbackOptions/FeedbackOption/FeedbackOptionStyled.jsx
+++ src/components/FeedbackOptions/FeedbackOption/FeedbackOptionStyled.jsx
@@ -16,16 +16,17 @@ const FeedbackOptionStyled = styled.button.attrs({
   font-weight: 600;
   text-transform: capitalize;
 
-  transition: box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1),
-    background-color 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1);
 
   &:hover,
   &:focus {
-    box-shadow: inset 0 0 0 1px #c3c3c3;
+    border-color: #88a7e0;
+    background-color: #bdd0f4;
   }
 
   &:active {
-    background-color: #c3c3c3;
+    border-color: #0c4fcc;
+    background-color: #0c4fcc;
   }
 `;
 
